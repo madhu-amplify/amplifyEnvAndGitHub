1dc9819bedd81b6e624c97bf0041c491784c03a5
Mohapatra <madmh@8c859064e286.ant.amazon.com>
2022-09-26 08:36:25 +1000

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,10 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "amplifyenvandgithub080c2dcb": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
